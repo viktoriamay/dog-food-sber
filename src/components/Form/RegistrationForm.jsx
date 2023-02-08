63f68c65f5a6eb54c88da57051646638a116222c
viktoriamay <alterkelly@mail.ru>
2023-02-08 23:09:16 +0300

    add example using useform

--- src/components/Form/RegistrationForm.jsx
+++ src/components/Form/RegistrationForm.jsx
@@ -4,10 +4,12 @@ import s from './Form.module.css';
 
 export const RegistrationForm = () => {
   
-  const {register, handleSubmit} = useForm();
+  const {register, handleSubmit, formState: {errors}} = useForm();
   const onSubmit = data => {
     console.log(data);
   }
+
+  console.log(errors);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -18,21 +20,37 @@ export const RegistrationForm = () => {
         // name='name' не нужно писать так как в поле реджистер в кавычках написали содержимое атрибута нейм
         placeholder='Имя'
         {...register('name', {
-          required: true,
-          maxLength: 5
+          required: "Обязательное поле",
+          maxLength: {
+            value: 10,
+            message: 'Слишком длинное имя'
+          }
         })}
         // value={contactInfo.name}
         // onChange={handleChange}
+        //ctrl shift i
       />
+      <div>
+        {errors?.name && <p>{errors?.name?.message}</p>}
+      </div>
       <input
         className={s.input}
         type='password'
         // name='password'
         placeholder='Пароль'
-        {...register('password')}
+        {...register('password', {
+          required: 'Обязательное поле',
+          minLength: {
+            value: 5,
+            message: 'Слишком короткий пароль'
+          }
+        })}
         // value={contactInfo.lastName}
         // onChange={handleChange}
       />
+      <div>
+        {errors?.password && <p>{errors?.password?.message}</p>}
+      </div>
       <input
         className={s.input}
         type='number'
