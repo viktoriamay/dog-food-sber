8e70a7796d92173a4e33d96db7cd27678ade39f4
viktoriamay <alterkelly@mail.ru>
2023-03-15 03:19:42 +0300

    refactor api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,74 +1,111 @@
 const onResponse = (res) => {
   return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
-}
+};
 
 class Api {
-  constructor({ baseUrl, headers, configFunc }) {
+  constructor({
+    baseUrl,
+    configFunc, 
+    // {headers} === config.headers
+  }) {
     this._baseUrl = baseUrl;
-    this._headers = headers;
     this._configFunc = configFunc;
+    // this._headers = headers;
   }
 
   getProductsList() {
-    return fetch(`${this._baseUrl}/products`, this._configFunc()).then(onResponse);
+    return fetch(`${this._baseUrl}/products`, this._configFunc()).then(
+      onResponse,
+    );
   }
 
   getUserInfo() {
-    return fetch(`${this._baseUrl}/users/me`, this._configFunc()).then(onResponse);
+    return fetch(`${this._baseUrl}/users/me`, this._configFunc()).then(
+      onResponse,
+    );
   }
 
   getProductById(idProduct) {
-    return fetch(`${this._baseUrl}/products/${idProduct}`, this._configFunc()).then(onResponse);
+    return fetch(
+      `${this._baseUrl}/products/${idProduct}`,
+      this._configFunc(),
+    ).then(onResponse);
   }
 
   deleteProductById(idProduct) {
-    return fetch(`${this._baseUrl}/products/${idProduct}`, { ...this._configFunc(), method: "DELETE" }).then(onResponse);
+    return fetch(`${this._baseUrl}/products/${idProduct}`, {
+      ...this._configFunc(),
+      method: 'DELETE',
+    }).then(onResponse);
   }
 
   search(searchQuery) {
-    return fetch(`${this._baseUrl}/products/search?query=${searchQuery}`, this._configFunc()).then(onResponse);
+    return fetch(
+      `${this._baseUrl}/products/search?query=${searchQuery}`,
+      this._configFunc(),
+    ).then(onResponse);
   }
 
   setUserInfo(dataUser) {
-    return fetch(`${this._baseUrl}/users/me`, { ...this._configFunc(), method: "PATCH", body: JSON.stringify(dataUser) }).then(onResponse);
+    return fetch(`${this._baseUrl}/users/me`, {
+      ...this._configFunc(),
+      method: 'PATCH',
+      body: JSON.stringify(dataUser),
+    }).then(onResponse);
   }
 
   changeLikeProduct(productId, isLike) {
-    return fetch(`${this._baseUrl}/products/likes/${productId}`, { ...this._configFunc(), method: isLike ? "DELETE" : "PUT" }).then(onResponse);
+    return fetch(`${this._baseUrl}/products/likes/${productId}`, {
+      ...this._configFunc(),
+      method: isLike ? 'DELETE' : 'PUT',
+    }).then(onResponse);
   }
 
   getUsersById(userId) {
-    return fetch(`${this._baseUrl}/v2/group-9/users/${userId}`, this._configFunc()).then(onResponse);
+    return fetch(
+      `${this._baseUrl}/v2/group-9/users/${userId}`,
+      this._configFunc(),
+    ).then(onResponse);
   }
 
   getUsers() {
-    return fetch(`${this._baseUrl}/v2/group-9/users`, this._configFunc()).then(onResponse);
+    return fetch(`${this._baseUrl}/v2/group-9/users`, this._configFunc()).then(
+      onResponse,
+    );
   }
 
   addReview(productId, body) {
-    return fetch(`${this._baseUrl}/products/review/${productId}`, { ...this._configFunc(), method: "POST", body: JSON.stringify(body) }).then(onResponse);
+    return fetch(`${this._baseUrl}/products/review/${productId}`, {
+      ...this._configFunc(),
+      method: 'POST',
+      body: JSON.stringify(body),
+    }).then(onResponse);
   }
 
   deleteReview(productId, reviewId) {
-    return fetch(`${this._baseUrl}/products/review/${productId}/${reviewId}`, { ...this._configFunc(), method: "DELETE" }).then(onResponse);
+    return fetch(`${this._baseUrl}/products/review/${productId}/${reviewId}`, {
+      ...this._configFunc(),
+      method: 'DELETE',
+    }).then(onResponse);
   }
 }
 
 const configFunc = () => {
   return {
     headers: {
-      "content-type": "application/json",
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
+      'content-type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
     },
   };
 };
 
 const config = {
-  baseUrl: "https://api.react-learning.ru",
-  headers: {
-    "content-type": "application/json",
-    Authorization: `Bearer ${localStorage.getItem("token")}`,
-  },
+  baseUrl: 'https://api.react-learning.ru',
+  // headers уже не нужен, так как он есть в configFunc
+  // headers: {
+  //   'content-type': 'application/json',
+  //   Authorization: `Bearer ${localStorage.getItem('token')}`,
+  // },
   configFunc: configFunc,
 };
 
